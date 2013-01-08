45ebfefbe2380648a41466d1da86c20c2f525f80
promotion.portoetenvirons <promotion.portoetenvirons@gmail.com>
2013-01-09 00:40:57 +0100

    insertion via jquery target _blank

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,7 @@
 console.log('This would be the main JS file.');
 
 $('#lien_externe p a').attr('target','_blank');
+
+ $(document).ready(function(){
+ $("a[@href^='http']").attr('target','_blank');
+});  

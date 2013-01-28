b7e7d21ec30c929293f6482629ca3baeba4d266f
holmeskelly78 <holmeskelly78@gmail.com>
2013-01-28 12:19:09 +0100

    correctif margin

--- _site/script_commit_promotionportoetenvirons.sh
+++ _site/script_commit_promotionportoetenvirons.sh
@@ -1,8 +1,10 @@
-git config --global user.name promotion-portoetenvirons
-git config --global user.email promotion.portoetenvirons@gmail.com
+#git config --global user.name promotion-portoetenvirons
+#git config --global user.email promotion.portoetenvirons@gmail.com
+git config --global user.name holmeskelly78
+git config --global user.email holmeskelly78@gmail.com
 git add downloads/ images/ _includes/ _site/
 git add _layouts/ _posts/ javascripts/
 git add stylesheets/ index.html _config.yml
-git commit -a -m "changement style small device"
+git commit -a -m "correctif margin"
 #git remote add promotionporto https://github.com/promotion-portoetenvirons/promotion-portoetenvirons.github.com.git
-git push -u promotionporto master
+git push -u origin master

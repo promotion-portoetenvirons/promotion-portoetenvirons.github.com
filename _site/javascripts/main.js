a736822ac9f194f59ace78c1e8248badd5b81fbe
holmeskelly78 <holmeskelly78@gmail.com>
2013-02-04 23:36:45 +0100

    correctif margin

--- _site/javascripts/main.js
+++ _site/javascripts/main.js
@@ -1,5 +1,3 @@
-console.log('This would be the main JS file.');
-
 $(document).ready(function(){
     $('#lien_externe p a').attr('target','_blank');
 });  
--- _site/script_commit_promotionportoetenvirons.sh
+++ _site/script_commit_promotionportoetenvirons.sh
@@ -1,9 +1,7 @@
 #git config --global user.name promotion-portoetenvirons
 #git config --global user.email promotion.portoetenvirons@gmail.com
-git config --global user.name holmeskelly78
-git config --global user.email holmeskelly78@gmail.com
 git add downloads/ images/ _includes/ _site/
-git add _layouts/ _posts/ javascripts/
+git add _layouts/ _posts/ javascripts/ php/
 git add stylesheets/ index.html _config.yml
 git commit -a -m "correctif margin"
 #git remote add promotionporto https://github.com/promotion-portoetenvirons/promotion-portoetenvirons.github.com.git

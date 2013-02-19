7c524e5550ec1a03df9842bc1a732e81fdf5aff6
promotion.portoetenvirons <promotion.portoetenvirons@gmail.com>
2013-02-19 13:27:05 +0100

    corrections orthographique gallerie photos

--- _posts/2012-10-25-image-habitation-agbodrafo-ppe.md
+++ _posts/2012-10-25-image-habitation-agbodrafo-ppe.md
@@ -3,6 +3,6 @@ layout: post
 categories: photosppe
 image: /images/image_habitation_agbodrafo.jpeg
 thumbnail: /images/thumbnails/image_habitation_agbodrafo.jpeg
-caption: Les habitations types d’Agbodrafo. Ce sont des maisons avec des cours intérieurs très agréables à vivre
+caption: Les habitations types d’Agbodrafo. Ce sont des maisons avec des cours intérieures très agréables à vivre
 imagetitle: Habitation Agbodrafo
 ---
--- _site/script_commit_promotionportoetenvirons.sh
+++
@@ -1,8 +0,0 @@
-#git config --global user.name promotion-portoetenvirons
-#git config --global user.email promotion.portoetenvirons@gmail.com
-git add downloads/ images/ _includes/ _site/
-git add _layouts/ _posts/ javascripts/ php/
-git add stylesheets/ index.html _config.yml
-git commit -a -m "correctif mail php"
-#git remote add origin https://github.com/promotion-portoetenvirons/promotion-portoetenvirons.github.com.git
-git push -u origin master
